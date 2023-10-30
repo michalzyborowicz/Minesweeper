3ee127c22e79b75428ad53719cdc86239e616d44
Michal Zyborowicz <mtzyb@MacBook-Pro.local>
2023-10-30 18:27:07 +0100

    Working on funciot that gonna create surten ammount of mines randomly placed on minefield

--- js/app.js
+++ js/app.js
@@ -7,15 +7,25 @@ const difficulty = document.querySelector('.difficulty')
 const gameBoard = document.querySelector('.gameboard')
 const minefield = document.querySelector('.minefield')
 const timer = document.querySelector('.timer')
-let time;
-let mines;
-const createMinefield = mines => {
-	for (let i = 0; i < mines; i++) {
+
+let time
+let mines
+const createMinefield = (cells, b) => {
+	for (let i = 0; i < cells; i++) {
 		const newCell = document.createElement('button')
 		minefield.appendChild(newCell)
 		newCell.classList.add('cell')
-	
-	}
+		newCell.setAttribute('data-id', i);
+
+		//Tutaj zacznij
+		let mines =Math.floor(Math.random(b) * cells)
+		for(let j = 0; j < b; j++){
+			
+			if(mines == j){
+				newCell.textContent = '💣'
+			}
+		}}
+
 	difficulty.classList.remove('active')
 	gameBoard.classList.add('active')
 }
@@ -35,6 +45,9 @@ const timeCount = time => {
 		time--
 	}, 1000)
 }
+function gameStart() {
+	get
+}
 
 const activeSwitch = () => {
 	rules.classList.remove('active')
@@ -42,20 +55,23 @@ const activeSwitch = () => {
 }
 
 btnRules.addEventListener('click', activeSwitch)
-btnEasy.addEventListener('click', () => {
-	createMinefield(64);
-    timeCount(600);
-	minefield.classList.add('easy');
+btnEasy.addEventListener('click', (e) => {
+	e.preventDefault()
+	createMinefield(64, 10)
+	timeCount(600)
+	minefield.classList.add('easy')
 })
-btnMedium.addEventListener('click', () => {
-	createMinefield(144);
-    timeCount(1200);
-	minefield.classList.add('medium');
+btnMedium.addEventListener('click', (e) => {
+	e.preventDefault()
+	createMinefield(144, 40)
+	timeCount(1200)
+	minefield.classList.add('medium')
 })
-btnHard.addEventListener('click', () => {
-	createMinefield(320);
-    timeCount(1800);
-	minefield.classList.add('hard');
+btnHard.addEventListener('click', (e) => {
+	e.preventDefault()
+	createMinefield(320, 99)
+	timeCount(1800)
+	minefield.classList.add('hard')
 })
 
-// @TODO: napisz funckje ktora bedzie losowo rozmieszczac miny i ustawiac je jako textContent
+//@TODO: ok, fajne miny sie wyswietlaja ale musi sie ich wyswietlac ilosc podana w w argumencie funckji a na razie wyswietla sie ich losowa liczba do 11 sztuk
